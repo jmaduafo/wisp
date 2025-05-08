baf0c6421a2d6ce85c6e1bf43453bcc801466861
Jasmine Maduafokwa <jmaduafokwa@hotmail.com>
2025-05-08 23:56:35 +0100

    added shadcn ui components and tweaked config files

--- src/components/pages/date-time/DateTime.tsx
+++ src/components/pages/date-time/DateTime.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Display from '../../ui/menu/Display'
+import { Nav } from '../../../types/types'
+import { CalendarDays, Calendar1, Clock8 } from 'lucide-react'
 
 function DateTime() {
+  // time, date
+  const data: Nav[] = [
+    {
+      navLink: "date",
+      icon: <Calendar1 strokeWidth={1} className='w-7 h-7'/>,
+      title: "date"
+    },
+    {
+      navLink: "time",
+      icon: <Clock8 strokeWidth={1} className='w-7 h-7'/>,
+      title: "time"
+    },
+    {
+      navLink: "calendar",
+      icon: <CalendarDays strokeWidth={1} className='w-7 h-7'/>,
+      title: "calendar"
+    },
+  ]
   return (
-    <div>DateTime</div>
+    <Display text='Date & time' array={data}/>
   )
 }
 
