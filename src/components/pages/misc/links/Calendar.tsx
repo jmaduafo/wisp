f1ca7f46fffe4b42f4d2125cdd2e570b402dcb74
Jasmine Maduafokwa <jmaduafokwa@hotmail.com>
2025-07-16 08:24:49 +0100

    reworked styling for calendar component

--- src/components/pages/misc/links/Calendar.tsx
+++ src/components/pages/misc/links/Calendar.tsx
@@ -1,8 +1,31 @@
-import Widget from '@/components/ui/widget/Widget'
-import React from 'react'
+import Widget from "@/components/ui/widget/Widget";
+import { Calendar as CalendarDisplay } from "@/components/ui/calendar";
+import { useAuth } from "@/context/AuthContext";
+import React from "react";
 
 export default function Calendar() {
+  const { userData } = useAuth();
+
+  const today = new Date();
+
   return (
-    <Widget>Calendar</Widget>
-  )
+    <Widget padding="p-2" className="overflow-y-auto overflow-x-hidden verticalScroll">
+      <CalendarDisplay
+        mode="single"
+        className="rounded-md"
+        captionLayout="dropdown"
+        modifiers={{
+          customToday: today,
+        }}
+        // Step 2: apply inline styles to that modifier
+        modifiersStyles={{
+          customToday: {
+            backgroundColor: userData?.secondary_color || "#fff",
+            color: userData?.primary_color || "#000",
+            borderRadius: "8px",
+          },
+        }}
+      />
+    </Widget>
+  );
 }
